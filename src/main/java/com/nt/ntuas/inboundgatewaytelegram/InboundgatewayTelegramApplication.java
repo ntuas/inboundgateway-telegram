3cac057ebd3bc31e991767f66acf7f09b4cbeeb2
Uwe Eisele <uwe.eisele@online.de>
2018-05-12 17:30:24 +0200

    Initial commit

---
+++ src/main/java/com/nt/ntuas/inboundgatewaytelegram/InboundgatewayTelegramApplication.java
@@ -0,0 +1,12 @@
+package com.nt.ntuas.inboundgatewaytelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboundgatewayTelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboundgatewayTelegramApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nt/ntuas/inboundgatewaytelegram/InboundgatewayTelegramApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nt.ntuas.inboundgatewaytelegram;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InboundgatewayTelegramApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
