8868e019d49a9af9cd17f66da1d4c002c1d59f72
Uwe Eisele <uwe.eisele-extern@deutschebahn.com>
2018-05-25 19:14:45 +0200

    trim commands

--- src/main/java/com/nt/ntuas/inboundgateway/telegram/api/model/TelegramBotCommandParser.java
+++ src/main/java/com/nt/ntuas/inboundgateway/telegram/api/model/TelegramBotCommandParser.java
@@ -11,9 +11,8 @@ public class TelegramBotCommandParser {
             normalized = text.substring(1, text.length());
         }
         int actionEndIdx = normalized.contains(" ") ? normalized.indexOf(" ") : normalized.length();
-        int argumentStartIndex = normalized.contains(" ") ? normalized.indexOf(" ") + 1 : normalized.length();
-        String action = normalized.substring(0, actionEndIdx);
-        String argument = normalized.substring(argumentStartIndex);
+        String action = normalized.substring(0, actionEndIdx).trim();
+        String argument = normalized.substring(actionEndIdx).trim();
         return new Command(action, argument);
     }
 }
